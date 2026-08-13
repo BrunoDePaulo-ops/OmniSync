579c6bd55c0496ca57bce4ff8194618a732ebd8d
BrunoDePaulo-ops <brunodepaulo@alu.ufc.br>
2026-08-13 15:02:19 -0300

    Update conexaoDAO.java

--- src/com/automacao/estoque/dao/conexaoDAO.java
+++ src/com/automacao/estoque/dao/conexaoDAO.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class conexaoDAO {
-    private static String URL = "jdbc:postgresql://localhost:5432/autostock";
+    private static String URL = "SUA_URL_AQUI";
     private static String usuario = "postgres";
-    private static String senha = "brunox123";
+    private static String senha = "SUA_SENHA_AQUI";
 
     public static Connection abrirConexao() throws SQLException{
         return DriverManager.getConnection(URL, usuario, senha);
